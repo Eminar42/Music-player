25491be87e5736159a2bad48def83a318e3e6155
Eminar42 <73086418+Eminar42@users.noreply.github.com>
2020-12-02 17:46:26 -0500

    Add files via upload

---
+++ mp3.py
@@ -0,0 +1,14 @@
+selection = input("What song do you want to listen to? ")
+def song_play(song):
+    with open('Song_playlist.txt', 'r') as playlist:
+        for songs in lines:
+            if selection == lines:
+                return selection
+            else:
+                pass
+
+
+
+
+print(song_play('song'))
+
